09b8e2d0ff6fc9bc271b9de85a5747588649a6f8
Jurgis Vosylius <v.jurgis.v@gmail.com>
2020-06-19 13:07:04 +0200

    Backing up important files

---
+++ PhoneCamera.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneCamera : MonoBehaviour
+{
+
+    private bool camAvailable;
+    private WebCamTexture backCam;
+    private Texture defaultBackground;
+
+    public RawImage background;
+    public AspectRatioFitter fit;
+    public bool forMobile;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        defaultBackground = background.texture;
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        // Check for at least 1 camera
+        if (devices.Length == 0)
+        {
+            Debug.Log("No Camera detected");
+            camAvailable = false;
+            return;
+        }
+
+        if (forMobile)
+        {
+            // Check for at least 1 back camera
+            for (int i = 0; i < devices.Length; i++)
+            {
+                if (!devices[i].isFrontFacing)
+                {
+                    backCam = new WebCamTexture(devices[i].name, Screen.width, Screen.height);
+                }
+            }
+        }
+        else
+        {
+            // Take the first cam
+            backCam = new WebCamTexture(devices[0].name, Screen.width, Screen.height);
+        }
+
+        // Check if backcam is null
+        if (backCam == null)
+        {
+            Debug.Log("Unable to find back camera");
+            return;
+        }
+
+        backCam.Play();
+        background.texture = backCam;
+
+        camAvailable = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!camAvailable)
+            return;
+
+        float ratio = (float)backCam.width / (float)backCam.height;
+        fit.aspectRatio = ratio;
+
+        // If yes then scaleY is -1f else 1f (swap pixels on y axis)
+        float scaleY = backCam.videoVerticallyMirrored ? -1f: 1f;
+        background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
+
+        int orient = -backCam.videoRotationAngle;
+        background.rectTransform.localEulerAngles = new Vector3(0, 0, orient);
+    }
+}
---
+++ SaveCameraPicture.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveCameraPicture : MonoBehaviour
+{
+    public Button takePictureButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = takePictureButton.GetComponent<Button>();
+        btn.onClick.AddListener(Snap);
+    }
+
+    // Take picture
+    void Snap()
+    {
+        Debug.Log("TODO: Take picture");
+    }
+}
